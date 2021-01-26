88d677521616e49de641ba0a7dfadfbb73e60c49
gabrielrsh <gabriel.shiota@gmail.com>
2021-01-26 16:19:05 -0300

    Implementados motor e direcao

---
+++ oo/carro.py
@@ -0,0 +1,146 @@
+"""
+Você deve criar uma classe carro que vai possuir
+dois atibutos compostos por outras duas  classes:
+
+1) Motor
+2) Direção
+
+O motor terá a responsabilidade de controlar a velocidade.
+Ele oferece os seguintes atributos:
+1) Atributo de dado velocidade
+2) Método acelerar, que deverá incrementar a velocidade de uma unidade
+3) Método frear, que deverá decrementar a velocidade em duas unidades
+
+A direção terá a responsabilidade de controlar a direção. Ela oferece
+os seguintes atributos:
+1) Valor de direção com valores possíveis: Norte, Sul, Leste, Oeste
+2) Método girar_a_direita
+3) Método girar_a_esquerda
+
+    N
+ O     L
+    S
+
+        Exemplo:
+        >>> #Testando Motor
+        >>> motor = Motor()
+        >>> motor.velocidade
+        0
+        >>> motor.acelerar()
+        >>> motor.velocidade
+        1
+        >>> motor.acelerar()
+        >>> motor.velocidade
+        2
+        >>> motor.acelerar()
+        >>> motor.velocidade
+        3
+        >>> motor.frear()
+        >>> motor.velocidade
+        1
+        >>> motor.frear()
+        >>> motor.velocidade
+        0
+        >>> #Testando Direção
+        >>> direcao = Direcao()
+        >>> direcao.valor
+        'Norte'
+        >>> direcao.girar_a_direita()
+        >>> direcao.valor
+        'Leste'
+        >>> direcao.girar_a_direita()
+        >>> direcao.valor
+        'Sul'
+        >>> direcao.girar_a_direita()
+        >>> direcao.valor
+        'Oeste'
+        >>> direcao.girar_a_direita()
+        >>> direcao.valor
+        'Norte'
+        >>> direcao.girar_a_esquerda()
+        >>> direcao.valor
+        'Oeste'
+        >>> direcao.girar_a_esquerda()
+        >>> direcao.valor
+        'Sul'
+        >>> direcao.girar_a_esquerda()
+        >>> direcao.valor
+        'Leste'
+        >>> direcao.girar_a_esquerda()
+        >>> direcao.valor
+        'Norte'
+        >>> carro = Carro(direcao, motor)
+        >>> carro.calcular_velocidade()
+        0
+        >>> carro.acelerar()
+        >>> carro.calcular_velocidade()
+        1
+        >>> carro.acelerar()
+        >>> carro.calcular_velocidade()
+        2
+        >>> carro.frear()
+        >>> carro.calcular_velocidade()
+        0
+        >>> carro.calcular_direcao()
+        'Norte'
+        >>> carro.girar_a_direita()
+        >>> carro.calcular_direcao()
+        'Leste'
+        >>> carro.girar_a_esquerda()
+        >>> carro.calcular_direcao()
+        'Norte'
+        >>> carro.girar_a_esquerda()
+        >>> carro.calcular_direcao()
+        'Oeste'
+"""
+
+class Motor:
+
+    def __init__(self):
+        self.velocidade = 0
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        self.velocidade -= 2
+        self.velocidade = max(0,self.velocidade)
+
+class Direcao:
+    def __init__(self):
+        self.valor = 'Norte'
+
+    def girar_a_direita(self):
+        dic = {'Norte':'Leste', 'Leste':'Sul', 'Sul':'Oeste', 'Oeste':'Norte'}
+        self.valor = dic[self.valor]
+
+    def girar_a_esquerda(self):
+        dic = {'Norte':'Oeste', 'Oeste':'Sul', 'Sul':'Leste', 'Leste':'Norte'}
+        self.valor = dic[self.valor]
+
+# class Carro:
+#
+#     def __init__(self, direcao, motor):
+#         self.motor = motor
+#         self.direcao = direcao
+#
+#     def calcular_velocidade(self):
+#         return self.motor.velocidade
+#
+#     def calcular_direcao(self):
+#         return self.direcao.valor
+
+    # if __name__ == '__main__':
+    #     motor = Motor()
+    #     direcao = Direcao()
+    #     print(motor.velocidade)
+    #     print(direcao.valor)
+    #     motor.acelerar()
+    #     print(motor.velocidade)
+    #     motor.frear()
+    #     print(motor.velocidade)
+    #     direcao.girar_a_direita()
+    #     print(direcao.valor)
+    #     direcao.girar_a_esquerda()
+    #     print(direcao.valor)
+    #     carro = Carro(direcao,motor)
