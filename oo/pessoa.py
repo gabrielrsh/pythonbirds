b5dc4cc9331b5acb4f4a033e4a9528a01e8216c8
gabrielrsh <gabriel.shiota@gmail.com>
2021-01-26 13:32:23 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=23):
+    def __init__(self, *filhos, nome=None, idade=23):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('gabriel')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Gabriel'
-    print(p.nome)
+    renzo = Pessoa(nome = 'Renzo')
+    luciano = Pessoa(renzo,nome = 'Luciano')
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho)
